729950d9f4309ce20685befc9bd5ac0f36977ae9
chenbaohe <baohechen@tencent.com>
2022-07-07 21:30:09 +0800

    207. 课程表

--- 力扣/Lc207canFinish.java
+++ 力扣/Lc207canFinish.java
@@ -2,9 +2,11 @@ package 力扣;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Lc207canFinish {
@@ -15,76 +17,37 @@ public class Lc207canFinish {
         System.out.println(new Lc207canFinish().canFinish(numCourses, prerequisites));
     }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if (numCourses <= 1 || prerequisites.length<=1){
-            return true;
-        }
+        List<int[]> reqList = new ArrayList<>();
+        Collections.addAll(reqList, prerequisites);
 
-        List<HashSet<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i<numCourses; i++){
-            graph.add(new HashSet<>());
-        }
+        while (reqList.size() > 0){
 
-        List<Integer> rootCourseList = new ArrayList<>();
-        for (int i = 0; i<numCourses; i++){
-            rootCourseList.add(i);
-        }
-
-        for (int[] req: prerequisites){
-            if (req[0] == req[1]) return false;
-            graph.get(req[0]).add(req[1]);
-            rootCourseList.remove(Integer.valueOf(req[1])); // remove value not index
-        }
-
-        if (rootCourseList.size() <= 0){
-            return false;
-        }
-
-        int[] dagLog = new int[numCourses];
-        for (int rootCourse: rootCourseList){
-            dagLog[rootCourse] = 1;
-            Set<Integer> traceLog = new HashSet<>();
-            traceLog.add(rootCourse);
-            if (hasLoop(graph, rootCourse, traceLog, dagLog)){
-                return false;
+            Set<Integer> outSet = new HashSet<>();
+            // 计算出度
+            for (int[] req: reqList){
+                int x = req[0];
+                outSet.add(x);
             }
-            traceLog.remove(rootCourse);
-        }
 
-        for (int i = 0; i<numCourses; i++){
-            if (dagLog[i] != 1){
-                return false;
+            // 寻找出度为 0 的课程
+            Set<Integer> out0set = new HashSet<>();
+            for (int[] req: reqList){
+                int y = req[1];
+                if (!outSet.contains(y)) out0set.add(y);
+            }
+
+            // 存在依赖关系,但没有出度为 0 的课程了: 有循环依赖,返回 false
+            if (out0set.size() == 0) return false;
+
+            // 从依赖关系中删除掉出度为 0 的依赖项
+            for (int i = reqList.size()-1; i>=0;i--){
+                int[] req = reqList.get(i);
+                if (out0set.contains(req[1])){
+                    reqList.remove(i);
+                }
             }
         }
+
         return true;
-    }
-
-    private boolean hasLoop(List<HashSet<Integer>> graph, int index, Set<Integer> traceLog, int[] dagLog){
-
-        if (graph.get(index).size() == 0) return false;
-        for (int visited: traceLog){
-            if (graph.get(index).contains(visited)){
-                return true;
-            }
-        }
-
-        for (int next: graph.get(index)){
-            traceLog.add(next);
-            dagLog[next] = 1;
-            boolean b = hasLoop(graph, next, traceLog, dagLog);
-            if (b) return true;
-            traceLog.remove(next);
-        }
-        return false;
-    }
-
-    private boolean bfs(int numCourses, int[][] prerequisites){
-        List<int[]> prereqList = new LinkedList<>();
-        Collections.addAll(prereqList, prerequisites);
-
-        Set<Integer> todo = new HashSet<>();
-        do{
-
-        }while (todo.size() != 0);
-        return false;
     }
 }
