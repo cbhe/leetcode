0ba14d0d6837f6464fb48a5718895574cbbfd3ca
chenbaohe <baohechen@tencent.com>
2022-07-11 14:11:54 +0800

    剑指 Offer 25. 合并两个排序的链表

---
+++ 剑指offer/Sro25mergeTwoLists.java
@@ -0,0 +1,28 @@
+package 剑指offer;
+
+import jdk.incubator.foreign.CLinker.VaList;
+import utils.list.ListNode;
+
+public class Sro25mergeTwoLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode p = l1, q = l2, pre = new ListNode(), tail = pre;
+        while (p!= null && q != null){
+                if (p.val <= q.val){
+                    tail.next = p;
+                    tail = tail.next;
+                    p = p.next;
+                } else {
+                    tail.next = q;
+                    tail = tail.next;
+                    q = q.next;
+                }
+        }
+        if (p != null) tail.next = p;
+        if (q != null) tail.next = q;
+
+        return pre.next;
+    }
+}
